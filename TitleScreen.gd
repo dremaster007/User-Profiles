6016e7fdd3882d7ed18728fdff925fc68687c860
dremaster007 <matosman1209@gmail.com>
2020-02-08 01:28:53 -0500

    Added the project to the GitHub repository

---
+++ TitleScreen.gd
@@ -0,0 +1,37 @@
+extends Control
+
+### FILE PATHS ARE NOT CASE SENSITIVE
+
+var user = {"username": "", 
+			"file": "",
+			"data": ""}
+
+func _ready():
+	pass
+	#for key in user.values():
+	#	print(key)
+	#print(user.keys())
+
+func setup():
+	var new_file = File.new()
+	
+	if new_file.file_exists(user["file"]):
+		new_file.open(user.get("file"), File.READ)
+		var content = new_file.get_as_text()
+		user["data"] = content
+		new_file.close()
+		print("test")
+	else:
+		new_file.open(user["file"], File.WRITE)
+		new_file.store_string("")
+		new_file.close()
+		#print_debug("No file existed with the name of: " + user["file"])
+		print_stack()
+	
+
+func _on_CreateUserButton_pressed():
+	user["username"] = $UsernameInput.text
+	user["file"] = "user://" + user["username"] + ".txt"
+	#print(user["username"])
+	#print(user["file"])
+	setup()
